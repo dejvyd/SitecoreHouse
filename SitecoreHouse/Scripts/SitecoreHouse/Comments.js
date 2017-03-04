7eb66ae00bc34db13686d4e9d778a8878652f7e7
Dawid Grybowicz <dgrybowicz@coders.center>
2017-03-04 14:32:05 +0100

    front-end changes

---
+++ SitecoreHouse/Scripts/SitecoreHouse/Comments.js
@@ -0,0 +1,27 @@
+﻿
+(function ($) {
+    if ($('body.pagemode-edit').length > 0)
+    {
+        //$('body').on("click", function (event) {
+        //    showSHComments();
+        //});
+
+        //function showSHComments() {
+        //    debugger;
+        //    log(event.target);
+        //    $(".SH-comments").addClass("ninja");
+        //    $(event.target).closest(".SH-comments").removeClass("ninja");
+        //    log($(event.target).closest(".SH-comments"));
+        //}
+
+        $.each($(".sh-comments"), function (key, value) {
+            $(this).parent().addClass('sh-comments-hover');
+        });
+
+        $('.sh-comments-hover').hover(function () {
+            $(this).closest('.sh-comments').removeClass('ninja');
+        }, function () {
+            $(this).closest('.sh-comments').addClass('ninja');
+        });
+    }
+})(jQuery);
