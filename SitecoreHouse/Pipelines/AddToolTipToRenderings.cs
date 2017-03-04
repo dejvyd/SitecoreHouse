97be02ae9d9a269b9e1abb8ac6ebd1dfb5c5c3ee
kkleszczewski <kkleszczewski@coders.center>
2017-03-04 08:41:55 +0100

    adding tooltips logic

---
+++ SitecoreHouse/Pipelines/AddToolTipToRenderings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.ExperienceEditor.Presentation;
+using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+using Sitecore.Mvc.Presentation;
+
+namespace SitecoreHouse.Pipelines
+{
+    public class AddToolTipToRenderings : RenderRenderingProcessor
+    {
+        public override void Process(RenderRenderingArgs args)
+        {
+            if (!string.IsNullOrEmpty(args.Rendering.DataSource) && args.PageContext.Item.Database.Name != "core" && args.PageContext.Item.Database.GetItem(args.Rendering.DataSource) == null)
+            {
+                Log.Warn(string.Format("'{0}' is not valid datasource.", (object)args.Rendering.DataSource), (object)this);
+                args.AbortPipeline();
+            }
+            else
+            {
+                if (args.Rendered || Context.Site == null || !Context.PageMode.IsExperienceEditorEditing)
+                    return;
+                IMarker marker = this.GetMarker();
+                if (marker == null)
+                    return;
+                int index = args.Disposables.FindIndex((Predicate<IDisposable>)(x => x.GetType() == typeof(Wrapper)));
+                if (index < 0)
+                    index = 0;
+                args.Disposables.Insert(index, (IDisposable)new Wrapper(args.Writer, marker));
+            }
+        }
+
+        protected virtual IMarker GetMarker()
+        {
+            RenderingContext currentOrNull1 = RenderingContext.CurrentOrNull;
+            if (currentOrNull1 == null || currentOrNull1.Rendering == null)
+                return (IMarker)null;
+            //if (!currentOrNull1.Rendering.IsXmlBasedRendering())
+            //    return (IMarker)null;
+            PlaceholderContext currentOrNull2 = PlaceholderContext.CurrentOrNull;
+            if (currentOrNull2 == null)
+                return (IMarker)null;
+            return (IMarker)null;
+            //return (IMarker)new RenderingMarker(currentOrNull1, currentOrNull2);
+        }
+    }
+}
