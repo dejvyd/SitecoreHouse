9709c8b87c0a8b28d2285c1e26b84c9b8ff7612b
Dawid Grybowicz <dgrybowicz@coders.center>
2017-03-04 07:07:21 +0100

    test wrapper

---
+++ SitecoreHouse/Models/TestWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+
+namespace SitecoreHouse.Models
+{
+    public class WraperTest : ExecuteRenderer
+    {
+        public override void Process(RenderRenderingArgs args)
+        {
+            if (args.Rendered || Context.Site == null || !Context.PageMode.IsPageEditorEditing || args.Rendering.RenderingType == "Layout")
+            {
+                return;
+            }
+
+            var marker = this.GetMarker(args);
+            if (marker == null)
+            {
+                return;
+            }
+
+            var index = args.Disposables.FindIndex(x => x.GetType() == typeof(Wrapper));
+            if (index < 0) index = 0;
+            args.Disposables.Insert(index, new Wrapper(args.Writer, marker));
+        }
+
+        protected virtual IMarker GetMarker(RenderRenderingArgs args)
+        {
+            // logic to create your Marker goes here
+        }
+    }
+}
